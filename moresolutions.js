a328948addbccc7b3023bd4fc8ae1be9eaddc2a4
Matt Valentine <codebymv@gmail.com>
2024-08-06 18:33:39 -0700

    more solutions updated

--- moresolutions.js
+++ moresolutions.js
@@ -3,4 +3,21 @@ function doubleInteger(i) {
     return i *2;
   }
 
-  
+
+
+
+//function that converts boolean values to their given strings
+function booleanToString(b){
+    return b.toString();
+  }
+
+
+
+//write a function that multiplies n * m and returns 0 if any values are below 0
+
+function paperwork(n, m) {
+    if (m < 0 || n < 0) {
+      return 0;
+    }
+    return m * n;
+  }
