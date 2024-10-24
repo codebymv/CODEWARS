c7564457d7a5bc0e0f9b187cf852d644f6585fa6
Matt <codebymv@gmail.com>
2024-10-23 23:14:01 -0700

    8kyu solution for string/array problem 10232024

---
+++ 8kyujoinmethodsolution10232024.js
@@ -0,0 +1,7 @@
+//function that takes array of words and uses join method with space seperator to combine them with a space between and then returns
+
+function smash (words) {
+    return words.join(' ')
+ };
+
+// the join method combines elements of an array into a single string and the space character inside of the string determines how they are combined
