e7ab277cbac3de6e910dcd3b25c032d567fe9e11
Matt <codebymv@gmail.com>
2024-10-24 20:08:49 -0700

    string to array problem 8kyu

---
+++ 8kyusolutionstringtoarray.js
@@ -0,0 +1,8 @@
+//declare an empty array, reassign the array to the string.split with space character as a seprator
+
+function stringToArray(string){
+    let arr = [];
+    arr = string.split(' ');
+    return arr
+    }
+
